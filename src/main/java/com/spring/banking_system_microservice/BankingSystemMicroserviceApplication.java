1563e5ba5e8738cd8b84bbf156b12629e54fc00e
Killian0812 <ngcuong0812@gmail.com>
2025-06-12 08:50:10 +0700

    spring initilzr

---
+++ src/main/java/com/spring/banking_system_microservice/BankingSystemMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.banking_system_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemMicroserviceApplication.class, args);
+	}
+
+}
