dd595c1c0a869ccddbfcf66ea74bde2cdfc70b46
Cuong Nguyen <ngcuong0812@gmail.com>
2025-06-14 21:59:07 +0700

    feat: eureka setup

--- src/main/java/com/spring/banking_system_microservice/BankingSystemMicroserviceApplication.java
+++ src/main/java/com/spring/banking_system_microservice/BankingSystemMicroserviceApplication.java
@@ -1,13 +1,27 @@
 package com.spring.banking_system_microservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@EnableEurekaServer
+// Temporary exclude JPA & Spring security
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+})
 public class BankingSystemMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankingSystemMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSystemMicroserviceApplication.class, args);
+    }
 
 }
